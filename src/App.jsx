2485116572e80dc09813776013abf34f03f663a4
= <--replace-all>
2025-03-13 15:50:41 +0500

    commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+function App() {
+  let users = [
+    { name: 'John', surname: 'Due' , age : 21 },
+    { name: 'Jane', surname: 'Doe' , age : 22 },
+    { name: 'Bob',  surname: 'Smith', age : 23 },
+    { name: 'Alice', surname: 'Johnson', age : 24 }
+  ]
+
+  return (
+
+    <div className="grid grid-cols-3 gap-3">
+      {users.map((user) => (
+
+      return(
+    <div>salom</div>
+  )
+      ))}
+  
+  
+
+     
+    </div>
+  )
+}
+
+export default App
